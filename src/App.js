713a0e182b397a5b4e8c4c84134c9e67f8f22f94
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-01-20 19:20:08 -0800

    comments

--- src/App.js
+++ src/App.js
@@ -9,15 +9,26 @@ class App extends Component {
     comments: [],
   };
 
-  addComment = () => {
-    /* change me */
+  addComment = (comment, author) => {
+    /* change me 
+    push to your comments state array. 
+    Then wire the addComment function 
+    up in context in the value object on line 20
+    you will need to change addComment to accept an argument
+    the comment addComment = (comment) => {...
+    because when you call addComment you will call it like this
+    this.context.addComment()
+    only you will need to pass the comment as an argument when you call it
+    {comment, author} is the object structure I believe*/
+
+    this.state.comments.push('')
   }
 
   render() {
     return (
       <CommentsContext.Provider
         value={{
-          comments: this.state.comments,
+          comments: this.context.addComment(comment, author),
         }}
       >
         <div className="App">
